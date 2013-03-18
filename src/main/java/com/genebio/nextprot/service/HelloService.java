c6ba0fe8f623ac57064f7363ec0631727b0a6e8d
Mario Pereira <mario.mpereira@gmail.com>
2013-03-18 16:27:21 +0100

    removed

---
+++ src/main/java/com/genebio/nextprot/service/HelloService.java
@@ -0,0 +1,5 @@
+package com.genebio.nextprot.service;
+
+public interface HelloService {
+	public String say();
+}
---
+++ src/main/java/com/genebio/nextprot/service/HelloServiceImpl.java
@@ -0,0 +1,10 @@
+package com.genebio.nextprot.service;
+
+public class HelloServiceImpl implements HelloService {
+
+	public String say() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
