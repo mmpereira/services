1fd81ee4136a37265e8110155748cd4b94263fe1
Mario Pereira <mario.mpereira@gmail.com>
2013-03-19 11:44:23 +0100

    added xml annotation

--- src/main/java/com/genebio/nextprot/domain/Publication.java
+++ src/main/java/com/genebio/nextprot/domain/Publication.java
@@ -1,8 +1,9 @@
 package com.genebio.nextprot.domain;
 
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 
-
+@XmlRootElement
 public class Publication {
 
 	private long id;
@@ -13,24 +14,31 @@ public class Publication {
 	public long getId() {
 		return id;
 	}
+	
 	public void setId(long id) {
 		this.id = id;
 	}
+	
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
 	public String getAbstractText() {
 		return abstractText;
 	}
+	
 	public void setAbstractText(String abstractText) {
 		this.abstractText = abstractText;
 	}
+	
 	public Date getPublicationDate() {
 		return publicationDate;
 	}
+	
 	public void setPublicationDate(Date publicationDate) {
 		this.publicationDate = publicationDate;
 	}
